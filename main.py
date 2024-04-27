95e927d4e70e09ccf29e2e8f5604b95889417db4
mzaninovic <mzaninovic555@gmail.com>
2024-04-27 18:58:59 +0200

    Added validation, slightly modified dataset

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from football_dataset import FootballDataset
 
 writer = SummaryWriter()
 
-dataset = FootballDataset('./data/Result_4.csv')
+dataset = FootballDataset('./data/Result_1.csv')
 
 input_features = dataset.__getitem__(0)[0].size(dim=0)
 output_features = dataset.__getitem__(0)[1].size(dim=0)
@@ -23,7 +23,11 @@ hidden_layer_size = floor(input_features * 2)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Device: {device}")
 
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+len_80 = int(len(dataset) * 0.8)
+len_20 = len(dataset) - len_80
+train, validation = torch.utils.data.random_split(dataset, [len_80, len_20])
+train_loader = DataLoader(train, batch_size=batch_size)
+validation_loader = DataLoader(validation, batch_size=batch_size)
 
 
 class NeuralNetwork(nn.Module):
@@ -38,9 +42,14 @@ class NeuralNetwork(nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
 
         self.sequential = nn.Sequential(
-            # hidden 1
+            # input
             nn.Linear(input_features, hidden_layer_size),
             self.hidden_func,
+
+            # hidden 1
+            nn.Linear(hidden_layer_size, hidden_layer_size),
+            self.hidden_func,
+            self.dropout,
 
             # hidden 2
             nn.Linear(hidden_layer_size, hidden_layer_size),
@@ -48,11 +57,6 @@ class NeuralNetwork(nn.Module):
             self.dropout,
 
             # hidden 3
-            nn.Linear(hidden_layer_size, hidden_layer_size),
-            self.hidden_func,
-            self.dropout,
-
-            # hidden 4
             nn.Linear(hidden_layer_size, hidden_layer_size),
             self.hidden_func,
 
@@ -78,7 +82,7 @@ def train():
 
         correct = 0
         total = 0
-        for data, target in dataloader:
+        for data, target in train_loader:
             data = data.to(device)
             target = target.to(device)
 
@@ -112,11 +116,12 @@ def train():
             if (i + 1) % 100 == 0:
                 print(
                     'Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {}'.format(
-                        epoch + 1, epochs, i, len(dataloader), loss.item(),
+                        epoch + 1, epochs, i, len(train_loader), loss.item(),
                         accuracy))
             i += 1
 
-    print(f"Training finished, total accuracy: {(total_correct / total_total) * 100}")
+    print(
+        f"Training finished, total accuracy: {(total_correct / total_total) * 100}")
 
     writer.flush()
     writer.close()
@@ -124,6 +129,39 @@ def train():
     now = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
     torch.save(model.state_dict(), f'./models/football_predictor_{now}_dict.pt')
     torch.save(model, f'./models/football_predictor_{now}_model.pt')
+
+    model.eval()
+
+    i = 0
+    valid_correct = 0
+    valid_total = 0
+    for data, target in validation_loader:
+        data = data.to(device)
+        target = target.to(device)
+
+        # Get predictions
+        output = model(data)
+
+        # Calculate loss
+        loss = criterion(output, target)
+
+        predicted = torch.argmax(output, dim=1)
+        predicted_one_hot = torch.nn.functional.one_hot(predicted,
+                                                        num_classes=3)
+
+        valid_correct += (predicted_one_hot == target).all(dim=1).sum()
+        valid_total += target.size(0)
+        accuracy = (valid_correct / valid_total) * 100
+
+        if (i + 1) % 100 == 0:
+            print(
+                'Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {}'.format(
+                    epoch + 1, epochs, i, len(validation_loader), loss.item(),
+                    accuracy))
+        i += 1
+
+    print(
+        f"Validation finished, total accuracy: {(valid_correct / valid_total) * 100}")
 
 
 if __name__ == '__main__':
