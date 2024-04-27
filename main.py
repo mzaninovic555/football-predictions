bbb52b6c2aee3a0d3208b41306e6f5e1b7d9f4f8
mzaninovic <mzaninovic555@gmail.com>
2024-04-27 12:13:39 +0200

    Updated gitignore, added tensorboard, model changes

--- main.py
+++ main.py
@@ -1,110 +1,137 @@
+import datetime
+from math import floor
+
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 from football_dataset import FootballDataset
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Device: {device}")
+writer = SummaryWriter()
 
 dataset = FootballDataset('./data/Result_4.csv')
 
 input_features = dataset.__getitem__(0)[0].size(dim=0)
 output_features = dataset.__getitem__(0)[1].size(dim=0)
 
-batch_size = 64
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+batch_size = 16
+epochs = 15
+learning_rate = 0.001
+dropout_rate = 0
+hidden_layer_size = floor(input_features * 1.5)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Device: {device}")
 
-epochs = 50
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(in_features=input_features,
-                      out_features=input_features * 3),
-            nn.ReLU(),
-            nn.Linear(in_features=input_features * 3,
-                      out_features=input_features * 3),
-            nn.ReLU(),
-            nn.Linear(in_features=input_features * 3,
-                      out_features=output_features),
-            nn.Sigmoid()
-        )
+
+        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU()
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+
+        self.dropout = nn.Dropout(dropout_rate)
+        self.l1 = nn.Linear(in_features=input_features,
+                            out_features=hidden_layer_size)
+        self.l2 = nn.Linear(in_features=hidden_layer_size,
+                            out_features=hidden_layer_size)
+        self.l3 = nn.Linear(in_features=hidden_layer_size,
+                            out_features=hidden_layer_size)
+        self.output = nn.Linear(in_features=hidden_layer_size,
+                                out_features=output_features)
 
     def forward(self, x):
-        logits = self.linear_relu_stack(x)
-        return logits
+        x = self.l1(x)
+        # x = self.sigmoid(x)
+        # x = self.tanh(x)
+        x = self.relu(x)
+        # x = self.dropout(x)
+        x = self.l2(x)
+#         x = self.sigmoid(x)
+#         x = self.tanh(x)
+        x = self.relu(x)
+#         x = self.dropout(x)
+#         x = self.l3(x)
+#         x = self.sigmoid(x)
+#         x = self.tanh(x)
+#         x = self.relu(x)
+        x = self.output(x)
+        return x
 
 
-model = NeuralNetwork().to(device)
-print(model)
+def train():
+    model = NeuralNetwork().to(device)
+    print(model)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.MSELoss()
+    # criterion = nn.BCEWithLogitsLoss()
 
-for epoch in range(epochs):
-    i = 0
+    optimizer = torch.optim.Adam(model.parameters())
+    # optimizer = torch.optim.Adamax(model.parameters(), lr=learning_rate)
+    # optimizer = torch.optim.NAdam(model.parameters(), lr=learning_rate)
+    # optimizer = torch.optim.Adadelta(model.parameters(), lr=learning_rate)
 
     total_correct = 0
-    total_samples = 0
+    total_total = 0
+    for epoch in range(epochs):
+        i = 0
 
-    for features, target in dataloader:
-        features = features.to(device)
-        target = target.to(device)
+        correct = 0
+        total = 0
+        for data, target in dataloader:
+            data = data.to(device)
+            target = target.to(device)
 
-        # Clear gradients
-        optimizer.zero_grad()
+            # Clear gradients
+            optimizer.zero_grad()
 
-        # Get predictions
-        output = model(features)
+            # Get predictions
+            output = model(data)
 
-        # Calculate loss
-        loss = criterion(output, target)
+            # Calculate loss
+            loss = criterion(output, target)
+            writer.add_scalar("Loss/train", loss, epoch)
 
-        # Backpropagate and update weights
-        loss.backward()
-        optimizer.step()
+            # Backpropagate and update weights
+            loss.backward()
+            optimizer.step()
 
-        total_correct += (output == target).sum().item()
-        total_samples += target.size(0)
+            predicted = torch.argmax(output, dim=1)
+            predicted_one_hot = torch.nn.functional.one_hot(predicted,
+                                                            num_classes=3)
 
-        # Print training progress (optional)
-        if i % 100 == 0:
-            accuracy = 100 * total_correct / total_samples
-            print(
-                'Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {}'.format(
-                    epoch + 1, epochs, i, len(dataloader), loss.item(),
-                    accuracy))
-        i += 1
+            correct += (predicted_one_hot == target).all(dim=1).sum()
+            total += target.size(0)
+            accuracy = (correct / total) * 100
 
-torch.save(model.state_dict(), './models/football_predictor.pt')
+            writer.add_scalar("Accuracy", accuracy, epoch)
 
-# current_position_distance
-# home_goals_scored
-# home_goals_conceded
-# home_goal_difference
-# away_goals_scored
-# away_goals_conceded
-# away_goal_difference
-# home_last_5_wins
-# home_last_5_draws
-# home_last_5_losses
-# away_last_5_wins
-# away_last_5_draws
-# away_last_5_losses
-# home_season_wins
-# home_season_draws
-# home_season_losses
-# away_season_wins
-# away_season_draws
-# away_season_losses
-# home_win
-# draw
-# away_win
+            total_correct += correct
+            total_total += total
 
-data = torch.tensor([1, 63, 37, 24, 77, 17, 60, 3, 1, 1, 3, 2, 0, 21, 6, 6, 27, 5, 1], dtype=torch.float).to(device)
-output = model(data)
-prediction = torch.argmax(output)
-print(f"Prediction: {output}")
+            if (i + 1) % 100 == 0:
+                print(
+                    'Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {}'.format(
+                        epoch + 1, epochs, i, len(dataloader), loss.item(),
+                        accuracy))
+            i += 1
+
+    print(f"Training finished, total accuracy: {(total_correct / total_total) * 100}")
+
+    writer.flush()
+    writer.close()
+
+    now = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    torch.save(model.state_dict(), f'./models/football_predictor_{now}_dict.pt')
+    torch.save(model, f'./models/football_predictor_{now}_model.pt')
+
+
+if __name__ == '__main__':
+    train()
